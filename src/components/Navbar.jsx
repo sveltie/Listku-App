162bb3b274740f67b04afc36d2ffc19b4f4a0f73
Yukii2k <lucydabestt@gmail.com>
2021-10-26 12:40:53 +0700

    Move styled components to separate file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import styled from 'styled-components';
-import { Link as LinkRouter } from 'react-router-dom';
+import { Wrapper, NavLogo } from "./components.style";
 import { useGlobalContext } from "../context/app.context";
+import { FaUserCircle } from "react-icons/fa"
 
 const Navbar = () => {
     const { user, logout } = useGlobalContext();
@@ -12,8 +12,8 @@ const Navbar = () => {
                 <NavLogo to='/'>LISTKU</NavLogo>
                 {user && (
                     <div className='nav-links'>
-                        <h5>Hello, {user}</h5>
-                        <button className='btn' onClick={logout}>
+                        <h5>Welcome, {user}</h5>
+                        <button className='btn btn-out' onClick={logout}>
                             logout
                         </button>
                     </div>
@@ -22,48 +22,6 @@ const Navbar = () => {
         </Wrapper>
     )
 }
-
-const Wrapper = styled.nav`
-  background: var(--primary-400);
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: sticky;
-
-  .nav-center {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-  }
-
-  .nav-links {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .nav-links h5 {
-    margin: 0;
-    margin-right: 2rem;
-  }
-   h5 {
-       color: white;
-   }
-`;
-
-const NavLogo = styled(LinkRouter)`
-    color: var(--white);
-    justify-self: flex-start;
-    cursor: pointer;
-    display: flex;
-    text-decoration: none;
-    font-size: 1.3rem;
-    letter-spacing: 3px;
-`
 
 
 export default Navbar;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from 'styled-components'
+import { EmptyContainer, Container } from "./components.style";
 import moment from "moment";
 import { Link } from 'react-router-dom';
 import { useGlobalContext } from "../context/app.context";
@@ -26,12 +26,12 @@ const Tasks = () => {
             {tasks.map((item) => {
                 const { _id: id, task, label, status, createdAt } = item;
                 let date = moment(createdAt);
-                date = date.format('dddd, MMMM Do YYYY, h:mm:ss a');
+                date = date.format('MMMM Do YYYY');
 
                 return (
                     <article key={id} className='task'>
                         <p className='task-date'>{date}</p>
-                        <h5>{task.toLowerCase()}</h5>
+                        <h5>{task}</h5>
                         <p className='task-label'>{label}</p>
                         <div className='task-links'>
                             <Link to={`/edit/${id}`} className='task-link'>
@@ -54,98 +54,5 @@ const Tasks = () => {
         </Container>
     )
 }
-
-const EmptyContainer = styled.section`
-  text-align: center;
-  h5 {
-    text-transform: none;
-  }
-`
-const Container = styled.section`
-  width: 90vw;
-  max-width: var(--max-width);
-  margin: 0 auto;
-  display: grid;
-  gap: 2rem 1rem;
-  grid-template-columns: 1fr 1fr 1fr;
-  padding-bottom: 5rem;
-
-  .task {
-    background: var(--white);
-    border-radius: var(--borderRadius);
-    padding: 2rem 1rem 1rem 1rem;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    box-shadow: var(--shadow-2);
-    transition: var(--transition);
-  }
-
-  .task h5 {
-    margin-bottom: 0;
-    text-transform: capitalize;
-    letter-spacing: 0;
-  }
-
-  .task-date {
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 0;
-    font-size: var(--smallText);
-    padding: 0.15rem 0.5rem;
-    border-top-right-radius: var(--borderRadius);
-    border-bottom-left-radius: var(--borderRadius);
-    background: var(--primary-100);
-    color: var(--primary-500);
-  }
-
-  .task-links {
-    display: flex;
-    align-items: center;
-    margin-top: auto;
-    font-size: 0.85rem;
-  }
-
-  .task-status {
-    margin-left: auto;
-    margin-bottom: 0;
-    margin-top: 0;
-    font-size: 0.85rem;
-  }
-  
-  .task-link {
-    background: transparent;
-    border: transparent;
-    text-transform: capitalize;
-    color: var(--green-dark);
-    cursor: pointer;
-    transition: var(--transition);
-    border-radius: var(--borderRadius);
-    line-height: 1;
-  }
-
-  .task-link:hover {
-    color: var(--primary-500);
-  }
-
-  .task-delete {
-    margin-left: 1rem;
-    color: var(--red-dark);
-  }
-
-  .task-status {
-    color: var(--primary-500);
-    text-transform: uppercase;
-  }
-
-  .task-status-declined {
-    color: var(--red-dark);
-  }
-
-  .task-status-interview {
-    color: var(--green-dark);
-  }
-`
 
 export default Tasks;
